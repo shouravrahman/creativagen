83c604029f7d584ec386e74265809036032d9ef5
shourav <shouravrahman006@gmail.com>
2024-07-13 04:37:05 +0600

    landing page enhancement

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,27 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
-import { BentoGrid } from "@/components/bento-grid";
 import Testimonials from "@/components/testimonials";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
-import Faq from "@/components/faqs/Faq.tsx";
-import Pricing from "@/components/pricing/Pricing.tsx";
 import Features from "@/components/features.tsx";
+import { PrimaryFeatures } from "./_components/PrimaryFeatures.tsx";
+import { SecondaryFeatures } from "./_components/SecondaryFeatures.tsx";
+import { Faqs } from "./_components/Faq.tsx";
+import { Pricing } from "./_components/Pricing.tsx";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
+      <PrimaryFeatures />
+      <SecondaryFeatures />
       <Features />
 
       <Pricing />
       <Testimonials />
       <Contact />
-      <Faq />
+      <Faqs />
 
       <Footer />
     </div>
