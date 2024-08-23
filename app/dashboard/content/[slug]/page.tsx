5fcbd5e563fc67be57619c201b78e72ad654b1ae
shourav <shouravrahman006@gmail.com>
2024-08-24 04:28:36 +0600

    icons and dynamic forms

--- app/dashboard/content/[slug]/page.tsx
+++ app/dashboard/content/[slug]/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 import FormSection from "@/components/content/FormSection";
 import ResultSection from "@/components/content/result-section.tsx";
+import { DynamicForm } from "@/components/DynamicForm";
+import { Heading } from "@/components/heading";
 import { Button } from "@/components/ui/button.tsx";
 import { TEMPLATES } from "@/constants.ts";
 import { TEMPLATE } from "@/types.ts";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, BookAIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-interface IContent {
+interface Content {
    params: { slug: string };
 }
-const ContentPage = ({ params }: IContent) => {
-   const selectedTemplate: TEMPLATE | undefined = TEMPLATES?.find(
+const ContentPage = ({ params }: Content) => {
+   const selectedTemplate: TEMPLATE = TEMPLATES?.find(
       (item) => item.slug === params.slug
    );
    console.log(selectedTemplate);
@@ -21,11 +23,11 @@ const ContentPage = ({ params }: IContent) => {
             <ArrowLeft className="h-6 w-6 mr-1" />
             <span className="text-lg">Back</span>
          </Link>
-         {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-10"> */}
-         <div className="grid grid-cols-1  md:px-10">
-            {/* form section */}
-            <FormSection selectedTemplate={selectedTemplate} />
-
+         <div className="grid grid-cols-1 mt-10 md:px-10">
+            {/* <Heading title={selectedTemplate?.name} description={selectedTemplate?.description} icon={<BookAIcon />} /> */}
+            <h1 className="text-2xl font-bold mb-5">{selectedTemplate?.name}</h1>
+            <p className="mb-8">{selectedTemplate?.description}</p>
+            <DynamicForm config={selectedTemplate} />
             {/* output section */}
             {/* <ResultSection /> */}
          </div>
