bb5686e63d2833ab52450d08fecc670b68b73522
shourav <shouravrahman006@gmail.com>
2024-10-03 03:41:49 +0600

    added more routes

--- routes.ts
+++ routes.ts
@@ -1,14 +1,20 @@
 export const publicRoutes: string[] = ["/verify", "/"];
 
 export const authRoutes: string[] = [
-  "/login",
-  "/register",
-  "/error",
-  "/resend",
-  "/reset",
-  "/new-password",
-  "/two-factor",
+	"/login",
+	"/register",
+	"/error",
+	"/resend",
+	"/reset",
+	"/new-password",
+	"/two-factor",
 ];
+export const protectedRoutes: string[] = [
+	"/dashboard",
+	"/dashboard/*",
+	"/dashboard/content/*",
+];
+export const adminRoutes: string[] = ["/admin"];
 
 export const apiAuthPrefix: string = "/api/auth";
 
