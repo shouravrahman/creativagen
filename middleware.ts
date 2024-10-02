47a0ee1505d2e0fbe63927862fa5ad3261272b85
shourav <shouravrahman006@gmail.com>
2024-10-03 03:41:42 +0600

    added protected and admin route check

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,54 @@
 import { authConfig } from "@/auth/config";
 import NextAuth from "next-auth";
 import {
-  DEFAULT_LOGIN_REDIRECT,
-  apiAuthPrefix,
-  authRoutes,
-  publicRoutes,
+	DEFAULT_LOGIN_REDIRECT,
+	apiAuthPrefix,
+	authRoutes,
+	protectedRoutes,
+	publicRoutes,
+	adminRoutes,
 } from "@/routes";
+import { NextAuthRequest } from "next-auth/lib";
 
 export const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
-  console.log(isLoggedIn);
+	const { nextUrl } = req;
+	const isLoggedIn = !!req.auth;
+	console.log("isLoggedIn", isLoggedIn);
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoutes = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
+	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+	const isPublicRoutes = publicRoutes.includes(nextUrl.pathname);
+	const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) {
-    return null;
-  }
+	if (isApiAuthRoute) {
+		return null;
+	}
 
-  if (isAuthRoutes) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return null;
-  }
+	if (isAuthRoutes) {
+		if (isLoggedIn) {
+			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+		}
+		return null;
+	}
+	if (protectedRoutes.includes(nextUrl.pathname)) {
+		if (!isLoggedIn) {
+			return Response.redirect(new URL("/login", nextUrl));
+		}
+	}
+	if (adminRoutes.includes(nextUrl.pathname)) {
+		if (!isLoggedIn) {
+			return Response.redirect(new URL("/login", nextUrl));
+		}
+		if (req.auth?.user.role !== "Admin") {
+			return Response.redirect(new URL("/", nextUrl));
+		}
+	}
+	if (!isLoggedIn && !isPublicRoutes) {
+		return Response.redirect(new URL("/login", nextUrl));
+	}
 
-  if (!isLoggedIn && !isPublicRoutes) {
-    return Response.redirect(new URL("/login", nextUrl));
-  }
-
-  return null;
+	return null;
 });
 
 // Optionally, don't invoke Middleware on some paths
