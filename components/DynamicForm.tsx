6b5460c49151a0f2895b2dd3a61cd8334651125d
shourav <shouravrahman006@gmail.com>
2024-11-12 05:03:37 +0600

    new form

--- components/DynamicForm.tsx
+++ components/DynamicForm.tsx
@@ -1,86 +1,185 @@
 "use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import { DynamicFormField } from "./FormField";
-import { v4 as uuidv4 } from "uuid";
-import { FormConfig } from "@/types";
 
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { createSchema } from "@/lib/utils";
+import { FormField } from "@/types";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+   Select,
+   SelectContent,
+   SelectItem,
+   SelectTrigger,
+   SelectValue
+} from "@/components/ui/select";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Slider } from "@/components/ui/slider";
+import { Button } from "@/components/ui/button";
+import {
+   Form,
+   FormControl,
+   FormDescription,
+   FormField as FormFieldWrapper,
+   FormItem,
+   FormLabel,
+   FormMessage,
+} from "@/components/ui/form";
+import { Card, CardContent } from "@/components/ui/card";
+
+import { useState } from "react";
 
 interface DynamicFormProps {
-	config: FormConfig;
-	submitFn: (values: Record<string, any>) => Promise<void>;
+   formFields: FormField[];
+   onSubmit: (data: Record<string, any>) => void;
+   initialEditorContent?: string;
 }
 
-export function DynamicForm({ config, submitFn }: DynamicFormProps) {
-	// Dynamically generate Zod schema based on form fields
-	const schemaFields = config.formFields.reduce((acc, field) => {
-		let fieldSchema;
-		switch (field.field) {
-			case "input":
-				fieldSchema = field.type === "number" ? z.number() : z.string();
-				break;
-			case "textarea":
-				fieldSchema = z.string();
-				break;
-			case "select":
-			case "radiogroup":
-				fieldSchema = z.enum(
-					field.options!.map((o) => o.value) as [string, ...string[]]
-				);
-				break;
-			case "switch":
-				fieldSchema = z.boolean();
-				break;
-			default:
-				fieldSchema = z.string();
-		}
-		if (!field.required) {
-			fieldSchema = fieldSchema.optional();
-		}
-		acc[field.name] = fieldSchema;
-		return acc;
-	}, {} as Record<string, z.ZodTypeAny>);
+const DynamicForm: React.FC<DynamicFormProps> = ({
+   formFields,
+   onSubmit,
+   initialEditorContent = ""
+}) => {
+   const [editorContent, setEditorContent] = useState(initialEditorContent);
 
-	const formSchema = z.object(schemaFields);
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
+   // const formSchema = z.object(schemaFields);
 
-		defaultValues: config.formFields.reduce((acc, field) => {
-			acc[field.name] = field.value !== undefined ? field.value : "";
-			return acc;
-		}, {} as Record<string, any>),
+   const form = useForm({
+   // resolver: zodResolver(formSchema),
 	});
 
-	const handleSubmit = async (values: z.infer<typeof formSchema>) => {
-		await submitFn(values);
+   const handleSubmit = async (values: any) => {
+      await onSubmit(values);
 	};
 
-	return (
-		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(handleSubmit)}
-				className="space-y-8"
-			>
-				<div className="grid grid-cols-1  w-full gap-6">
-					{config.formFields.map((fieldConfig) => (
-						<DynamicFormField
-							key={uuidv4()}
-							config={fieldConfig}
-						/>
-					))}
-				</div>
-				<Button
-					type="submit"
-					variant="destructive"
-					className="w-full text-lg"
-				>
-					Generate
-				</Button>
-			</form>
-		</Form>
-	);
-}
+
+   return (
+      <div className="flex  lg:min-w-[40%] gap-6 mt-6  ">
+         <Card className="p-6 w-full">
+            <Form {...form}>
+               <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 ">
+                  {formFields?.sort((a, b) => a.order - b.order).map((field) => (
+                     <FormFieldWrapper
+                        key={field.name}
+                        control={form.control}
+                        name={field.name}
+                        render={({ field: formField }) => (
+                           <FormItem>
+                              <FormLabel>{field.label}</FormLabel>
+                              <FormControl>
+                                 {(() => {
+                                    switch (field.type) {
+                                       case "text":
+                                          return (
+                                             <Input
+                                                {...formField}
+                                                placeholder={field.helpText}
+                                                required
+                                             />
+                                          );
+                                       case "textarea":
+                                          return (
+                                             <Textarea
+                                                {...formField}
+                                                placeholder={field.helpText}
+                                                className="min-h-[100px]"
+                                                required
+                                             />
+                                          );
+                                       case "select":
+                                          return (
+                                             <Select
+                                                onValueChange={formField.onChange}
+                                                defaultValue={formField.value}
+                                                required
+                                             >
+                                                <SelectTrigger>
+                                                   <SelectValue placeholder={field.helpText || "Select an option"} />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                   {field.options?.map((option) => (
+                                                      <SelectItem
+                                                         key={option.value}
+                                                         value={option.value}
+                                                      >
+                                                         {option.label}
+                                                      </SelectItem>
+                                                   ))}
+                                                </SelectContent>
+                                             </Select>
+                                          );
+                                       case "radio":
+                                          return (
+                                             <RadioGroup
+                                                onValueChange={formField.onChange}
+                                                defaultValue={formField.value}
+                                                className="flex flex-col space-y-1"
+                                                required
+                                             >
+                                                {field.options?.map((option) => (
+                                                   <div key={option.value} className="flex items-center space-x-2">
+                                                      <RadioGroupItem value={option.value} id={option.value} />
+                                                      <label
+                                                         htmlFor={option.value}
+                                                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                      >
+                                                         {option.label}
+                                                      </label>
+                                                   </div>
+                                                ))}
+                                             </RadioGroup>
+                                          );
+                                       case "slider":
+                                          return (
+                                             <div className="space-y-4">
+                                                <Slider
+
+                                                   onValueChange={(value) => formField.onChange(value[0])}
+                                                   defaultValue={[formField.value]}
+                                                   min={field.min || 0}
+                                                   max={field.max || 100}
+                                                   step={field.step || 1}
+                                                   className="w-full"
+                                                />
+                                                <div className="text-xs text-muted-foreground text-right">
+                                                   Value: {formField.value}
+                                                </div>
+                                             </div>
+                                          );
+                                       case "number":
+                                          return (
+                                             <Input
+                                                required
+                                                type="number"
+                                                {...formField}
+                                                placeholder={field.helpText}
+                                                min={field.min}
+                                                max={field.max}
+                                                step={field.step}
+                                             />
+                                          );
+                                       default:
+                                          return null;
+                                    }
+                                 })()}
+                              </FormControl>
+                              {field.helpText && (
+                                 <FormDescription>{field.helpText}</FormDescription>
+                              )}
+                              <FormMessage />
+                           </FormItem>
+                        )}
+                     />
+                  ))}
+                  <Button type="submit" className="bg-accent text-xl font-semibold w-full">Generate Content</Button>
+               </form>
+            </Form>
+         </Card>
+      </div>
+   );
+};
+
+export default DynamicForm;
