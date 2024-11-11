b71946bd6d18ee264bb0b19526a850d429a35d19
shourav <shouravrahman006@gmail.com>
2024-11-12 05:03:04 +0600

    image change

--- app/(landing)/_components/LandingHero.tsx
+++ app/(landing)/_components/LandingHero.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import hero from "../../../public/hero2.png";
+import hero from "../../../public/heross.png";
 
 export const LandingHero = () => {
 	// const user = await currentUser();
