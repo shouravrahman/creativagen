a402ece1c63e92c765ec81f65fd6b90478e7e114
shourav <shouravrahman006@gmail.com>
2024-11-13 02:45:10 +0600

    padding

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -1,25 +1,20 @@
 
-import { MobileSidebar } from "@/components/mobile-sidebar";
-import { getApiLimitCount } from "@/lib/api-limit";
-import { checkSubscription } from "@/lib/subscription";
 import ThemeSwitch from "./ThemeSwitch.tsx";
 import IconDropdown from "./IconDropdown.tsx";
-import { getSession } from "next-auth/react";
 import { auth } from "@/auth/index.ts";
 export interface IMenuItem {
    label: string;
    href: string;
 }
 const Navbar = async () => {
-   const apiLimitCount = await getApiLimitCount();
-   const isPro = await checkSubscription();
+
    const session = await auth()
 
    const menu = [
       { label: "Profile", href: '/profile' }
    ]
    return (
-		<div className="flex items-center px-4 my-2 w-full ">
+      <div className="flex items-center px-4 mt-1 w-full ">
 			{/* <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} /> */}
 			<div className="flex w-full justify-end gap-6">
 				<ThemeSwitch />
